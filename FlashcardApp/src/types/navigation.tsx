5a5be9a179e3e65513fdfbae5642c4b125c5fae9
Woody Lin <wancheng.lin001@umb.edu>
2024-10-07 22:43:36 -0400

    Update navigation.tsx

--- FlashcardApp/src/types/navigation.tsx
+++ FlashcardApp/src/types/navigation.tsx
@@ -1,6 +1,8 @@
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RouteProp } from '@react-navigation/native';
 
+// RootStackParamList: This defines the structure of your navigation stack. 
+// It lists all the screens in your app and the parameters they can receive.
 export type RootStackParamList = {
   Home: undefined;
   Deck: { deckId: number };
@@ -9,12 +11,14 @@ export type RootStackParamList = {
   EditCard: { deckId: number; cardId?: number };
 };
 
+// This ensures that you're using the correct navigation methods and passing the right parameters when navigating.
 export type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Home'>;
 export type DeckScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Deck'>;
 export type FlashcardScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Flashcard'>;
 export type CreateDeckScreenNavigationProp = StackNavigationProp<RootStackParamList, 'CreateDeck'>;
 export type EditCardScreenNavigationProp = StackNavigationProp<RootStackParamList, 'EditCard'>;
 
+// This is useful for accessing parameters passed to the screen.
 export type DeckScreenRouteProp = RouteProp<RootStackParamList, 'Deck'>;
 export type FlashcardScreenRouteProp = RouteProp<RootStackParamList, 'Flashcard'>;
 export type EditCardScreenRouteProp = RouteProp<RootStackParamList, 'EditCard'>;
